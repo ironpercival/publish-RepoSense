2cab21f002b3889aa11d87dcf69d70a4a2d5de33
Anton Shabinskiy <a@bnsk.pro>
2022-12-24 10:31:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@
 ### ./get-reposense.py --commit abc123         # Gets a specific commit
 ### ./get-reposense.py --release --overwrite   # Overwrite RepoSense.jar, if exists, with the latest release
 
-./get-reposense.py --master
+./get-reposense.py --master --since 1/9/2022
 
 # Executes RepoSense
 # Do not change the default output folder name (reposense-report)
